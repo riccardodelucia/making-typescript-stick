b4810f5746e6739ed89e6b5e2aa787716cd363bf
Riccardo Roberto De Lucia <riccardoroberto.delucia@gmail.com>
2024-07-29 15:05:48 +0200

    jquery-clone completed

--- challenges/jquery-clone/src/index.ts
+++ challenges/jquery-clone/src/index.ts
@@ -1,12 +1,54 @@
 import fetch from "node-fetch";
 
-function $(selector: string): any {
-  return {} as any;
+class SelectorResult {
+  #elements;
+  constructor(elements: NodeListOf<Element>) {
+    this.#elements = elements;
+  }
+  html(contents: string) {
+    this.#elements.forEach((el) => (el.innerHTML = contents));
+  }
+  on<K extends keyof HTMLElementEventMap>(
+    eventName: K,
+    callback: (event: HTMLElementEventMap[K]) => void
+  ) {
+    this.#elements.forEach((el) => {
+      if (el instanceof HTMLElement) {
+        el.addEventListener(eventName, callback);
+      }
+    });
+  }
+  hide() {
+    this.#elements.forEach((el) => {
+      if (el instanceof HTMLElement) {
+        el.style.visibility = "hidden";
+      }
+    });
+  }
+  show() {
+    this.#elements.forEach((el) => {
+      if (el instanceof HTMLElement) {
+        el.style.visibility = "visible";
+      }
+    });
+  }
+}
+
+function $(selector: string): SelectorResult {
+  return new SelectorResult(document.querySelectorAll(selector));
 }
 
 namespace $ {
-  export function ajax(...args: any[]): any {
-    return {} as any;
+  export function ajax({
+    url,
+    successCB,
+  }: {
+    url: string;
+    successCB: (data: any) => void;
+  }): any {
+    return fetch(url)
+      .then((resp) => resp.json())
+      .then(successCB);
   }
 }
 
